c74eaaee65a63f3c08aec6a06f4f12bd00e29a5d
KRocioCC <krociocalderon@gmail.com>
2026-08-24 10:57:37 -0400

    estructura basica y rutas definidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'basic',
+        title: 'Pipes Basicos',
+        loadComponent: () => import('./pages/basic-page/basic-page'),
+    },
+    {
+        path: 'custom',
+        title: 'custom pipes personalizados',
+        loadComponent: () => import('./pages/custom-page/custom-page'),
+    },
+    {
+        path: 'uncommon',
+        title: 'Pipes no tan comunes',
+        loadComponent: () => import('./pages/uncommon-page/uncommon-page'),
+    },
+    {
+        path: 'numbers',
+        title: 'Numbers Pipe',
+        loadComponent: () => import('./pages/numbers-page/numbers-page'),
+    }
+];
